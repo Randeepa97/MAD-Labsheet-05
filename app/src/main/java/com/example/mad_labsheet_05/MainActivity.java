f05d6b626151015d59e88ae030b410b163399404
Randeepa Bandara <42823841+Randeepa97@users.noreply.github.com>
2020-05-28 21:08:31 +0530

    2nd commit

--- app/src/main/java/com/example/mad_labsheet_05/MainActivity.java
+++ app/src/main/java/com/example/mad_labsheet_05/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.mad_labsheet_05;
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.icu.text.BreakIterator;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String BROADCAST_ACTION = null;
+    Button button;
+    private Receiver localListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button= findViewById(R.id.button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        localListener = new Receiver();
+        IntentFilter intentFilter = new IntentFilter(BROADCAST_ACTION);
+        this.registerReceiver(localListener);
+    }
+
+    private void registerReceiver(Receiver localListener) {
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.unregisterReceiver(localListener);
+    }
+
+    public void onClick(View view){
+        if(view.getId() == R.id.button){
+            BackgroundServices.startAction(this.getApplicationContext());
+        }
+    }
+
+    public class Receiver extends BroadcastReceiver{
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            BreakIterator txtViewMsg = null;
+            String currentText = txtViewMsg.getText().toString();
+            String message = intent.getStringExtra("value");
+            currentText += "\nReceived " + message;
+            txtViewMsg.setText(currentText);
+        }
     }
 }
